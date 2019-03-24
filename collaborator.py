d58cf7bacc3c3ecef6172873fe6c4594239c97fd
Mohammed Salam <34826407+Msalam96@users.noreply.github.com>
2019-03-24 16:40:00 -0400

    Add files via upload

--- collaborator.py
+++ collaborator.py
@@ -50,8 +50,10 @@ def userexists(f, l):
     row = c.rowcount
     return row == 1
 
+
 def custom_sort(t):
     return t[4]
+
 
 def sim_interests():
     data_interest = []
@@ -79,6 +81,7 @@ def sim_interests():
     else:
         print("-> Sorry, this user does not exist.")
 
+
 def sim_skills():
     data_interest = []
     first, last = input("Enter the first and last name of the user.\n-> ").split()
@@ -105,20 +108,88 @@ def sim_skills():
     else:
         print("-> Sorry, this user does not exist.")
 
-# def trusted_coll():
-#     first, last = input("Enter the first and last name for whom you would like to find trusted colleagues. \n ").split()
-#     if userexists(first, last):
-#         c.execute("SELECT * FROM user WHERE first=%s AND last=%s", (first, last))
-#         user_info = c.fetchone()
-#         c.execute("SELECT org_name FROM organization WHERE user_id=%s", (user_info[0],))
-#         user_org = c.fetchone()
-#         c.execute("SELECT user_id FROM organization WHERE org_name=%s", (user_org[0],))
-#         coworkers = c.fetchall()
-#         for each coworker in coworkers:
-#             c.execute("SELECT ")
-#
-#     else:
-#         print("-> Sorry, this user does not exist.")
+
+def details():
+    cur = conn.cursor()
+    try:
+        first, last = input("Enter the first and last name of the user.\n-> ").split()
+        if userexists(first, last):
+            cur.execute("SELECT * FROM user WHERE first=%s AND last=%s", (first, last))
+            userset = (cur.fetchone())
+            uid = userset[0]
+            #print('-> The users id is: ', userset[0])
+            print('-> The users first name is: ', userset[1])
+            print('-> The users last name is: ', userset[2])
+            # print('-> The users phone number is: ', userset[3])
+            # print('-> The users address is: ', userset[4])
+            # print('-> The user has a degree in: ', userset[5])
+            query = "SELECT * FROM project WHERE user_id=%s"
+            cur.execute(query, (uid,))
+            userset2 = cur.fetchall()
+            print("-> Projects this user has worked on are: ")
+            for row in userset2:
+                print("-> ", row[1])
+            query2 = "SELECT * FROM interest WHERE user_id=%s"
+            cur.execute(query2, (uid,))
+            userset3 = cur.fetchall()
+            print("-> Interests this user has are: ")
+            for row2 in userset3:
+                print("-> ", row2[1])
+            query3 = "SELECT * FROM skill WHERE user_id=%s"
+            cur.execute(query3, (uid,))
+            userset4 = cur.fetchall()
+            print("-> Skills this user has are: ")
+            for row3 in userset4:
+                print("-> ", row3[1])
+            query4 = "SELECT * FROM organization WHERE user_id=%s"
+            cur.execute(query4, (uid,))
+            userset5 = cur.fetchone()
+            print("-> This user's organization is: ", userset5[1])
+        else:
+            print("-> Sorry, this user does not exist.")
+    except ValueError:
+        print("-> This input is invalid.")
+
+
+def collaborators():
+    cur = conn.cursor()
+    try:
+        first, last = input("Enter the first and last name of the user to collaborate with.\n-> ").split()
+        if userexists(first, last):
+            query = """SELECT user.user_id, user.first, user.last, project.user_id, project.proj_name
+                        FROM user, project
+                        WHERE first=%s AND last=%s AND user.user_id = project.user_id
+                        ORDER BY user.user_id"""
+            cur.execute(query, (first, last))
+            rows = cur.fetchall()
+            for row in rows:
+                idv = row[0]
+                ids = []
+                query2 = """SELECT * FROM project WHERE proj_name IN(
+                            SELECT proj_name FROM project
+                            WHERE proj_name=%s
+                            GROUP BY proj_name HAVING count(*) > 1 )"""
+                cur.execute(query2, (row[4],))
+                rows2 = cur.fetchall()
+                for row2 in rows2:
+                    ids.append(row2[0])
+                query3 = "SELECT * FROM user WHERE user_id=%s"
+                print("-> Here is a list of trusted colleague(s) for project:", row[4], ":")
+                for x in range(len(ids)):
+                    cur.execute(query3, (ids[x],))
+                    rows3 = cur.fetchall()
+                    count = cur.rowcount
+                    for row3 in rows3:
+                        if idv == row3[0]:
+                            pass
+                        elif count == 0:
+                            print("-> No Collaborators")
+                        else:
+                            print("-> ", row3[1], row3[2])
+        else:
+            print("-> Sorry this user does not exist.")
+    except ValueError:
+        print("This input is valid.")
 
 
 user_data = csv.reader(open('user.csv'))
@@ -140,20 +211,14 @@ for row in user_data:
         firstline = False
         continue
     else:
-        c.execute('INSERT INTO user(user_id, \
-                first, last )' \
-                'VALUES(%s, %s, %s)',
-                row)
+        c.execute('INSERT INTO user(user_id,first, last ) VALUES(%s, %s, %s)', row)
 
 for row in project_data:
     if firstline2:
         firstline2 = False
         continue
     else:
-        c.execute('INSERT INTO project(user_id, \
-                proj_name)' \
-                'VALUES(%s, %s)',
-                row)
+        c.execute('INSERT INTO project(user_id, proj_name) VALUES(%s, %s)', row)
 
 for row in interest_data:
     if firstline3:
@@ -185,66 +250,22 @@ for row in dist_data:
 
 print("Welcome to the Collaborator Software. The CSV files have been read! Here are the following inputs:")
 while True:
-    key = input(""" 
+    key = int(input(""" 
 Press 1 to find information about a specific user. 
 Press 2 to find collaborators to a user. 
 Press 3 to find a user with similar interests.
 Press 4 to find a user with a common skill.
 Press 5 to find a trusted colleague. 
-Press 0 to exit \n-> """)
-    if key == '1':
-        cur = conn.cursor()
-        first, last = input("Enter the first and last name of the user.\n-> ").split()
-        if userexists(first,last):
-            cur.execute("SELECT * FROM user WHERE first=%s AND last=%s", (first, last))
-            userset = (cur.fetchone())
-            print('-> The users id is: ', userset[0])
-            print('-> The users first name is: ', userset[1])
-            print('-> The users last name is: ', userset[2])
-            # print('-> The users phone number is: ', userset[3])
-            # print('-> The users address is: ', userset[4])
-            # print('-> The user has a degree in: ', userset[5])
-        else:
-            print("-> Sorry, this user does not exist.")
-    elif key == '2':
-        cur = conn.cursor()
-        first, last = input("Enter the first and last name of the user to collaborate with.\n-> ").split()
-        if userexists(first,last):
-            query = """SELECT user.user_id, user.first, user.last, project.user_id, project.proj_name
-                FROM user, project
-                WHERE first=%s AND last=%s AND user.user_id = project.user_id
-                ORDER BY user.user_id"""
-            cur.execute(query, (first, last))
-            rows = cur.fetchall()
-            for row in rows:
-                idv = row[0]
-                proj = row[4]
-                ids = []
-                query2 = """SELECT * FROM project WHERE proj_name IN(
-                    SELECT proj_name FROM project
-                    WHERE proj_name=%s
-                    GROUP BY proj_name HAVING count(*) > 1 )"""
-                cur.execute(query2, (row[4],))
-                rows2 = cur.fetchall()
-                for row2 in rows2:
-                    ids.append(row2[0])
-                query3 = "SELECT * FROM user WHERE user_id=%s"
-                print("-> Here is a list of trusted colleague(s) for project", row[4], ":")
-                for x in range(len(ids)):
-                    cur.execute(query3, (ids[x],))
-                    rows3 = cur.fetchall()
-                    for row3 in rows3:
-                        if idv == row3[0]:
-                            pass
-                        else:
-                            print("-> ", row3[1], row3[2])
-        else:
-            print("-> Sorry this user does not exist.")
-    elif key == '3':
+Press 0 to exit \n-> """))
+    if key == 1:
+        details()
+    elif key == 2:
+        collaborators()
+    elif key == 3:
         sim_interests()
-    elif key == '4':
+    elif key == 4:
         sim_skills()
-    elif key == '0':
+    elif key == 0:
         print("Thank you for using our software")
         break
     else:
